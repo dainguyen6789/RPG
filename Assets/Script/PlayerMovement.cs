326e95240916fad628b8dd5938610283c8c55ade
DESKTOP-TR0VMRU\pc <ngdai6789@gmail.com>
2017-09-30 20:42:48 -0400

    CameraRayCaster and Mouse Control Feature Added

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof (ThirdPersonCharacter))]
+public class PlayerMovement : MonoBehaviour
+{
+    ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
+    CameraRaycaster cameraRaycaster;
+    Vector3 currentClickTarget;
+        
+    private void Start()
+    {
+        cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+        m_Character = GetComponent<ThirdPersonCharacter>();
+        currentClickTarget = transform.position;
+    }
+
+    // Fixed update is called in sync with physics
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+			
+            print("Cursor raycast hit" + cameraRaycaster.hit.collider.gameObject.name.ToString());
+			switch (cameraRaycaster.layerHit) {
+			case Layer.Walkable:
+				currentClickTarget = cameraRaycaster.hit.point;  // So not set in default case
+				break;
+			case Layer.Enemy:
+				print ("Not moving to enemy");
+				break;
+			}
+        }
+		m_Character.Move (3*currentClickTarget - 3*transform.position, false, false);
+
+    }
+}
+
